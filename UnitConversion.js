43e7402f10082675682cc8ca77ee388227b193ed
Karthik <karthikr.1703@gmail.com>
2020-10-12 20:23:36 +0530

    Add Weight Unit

--- UnitConversion.js
+++ UnitConversion.js
@@ -104,3 +104,66 @@ function Lenght_Base_ToUnit_ConversionFactor(toUnit) {
 
   return retVal;
 }
+
+function Weight_Conversion(fromUnit, toUnit, fromValue) {
+  var conversionValue = Weight_FromUnit_Base_ConversionFactor(fromUnit);
+  var baseToValue = fromValue * conversionValue;
+  conversionValue = Weight_Base_ToUnit_ConversionFactor(toUnit);
+  return baseToValue * conversionValue;
+}
+
+function Weight_FromUnit_Base_ConversionFactor(fromUnit) {
+  var retVal = 0;
+  switch (fromUnit) {
+    case "mg":
+      retVal = 0.000001;
+      break;
+    case "g":
+      retVal = 0.001;
+      break;
+    case "kg":
+      retVal = 1;
+      break;
+    case "t":
+      retVal = 1000;
+      break;
+    case "lb":
+      retVal = 1 / 2.205;
+      break;
+    case "oz":
+      retVal = 1 / 35.274;
+      break;
+    default:
+      retVal = 0;
+  }
+
+  return retVal;
+}
+
+function Weight_Base_ToUnit_ConversionFactor(fromUnit) {
+  var retVal = 0;
+  switch (fromUnit) {
+    case "mg":
+      retVal = 1000000;
+      break;
+    case "g":
+      retVal = 1000;
+      break;
+    case "kg":
+      retVal = 1;
+      break;
+    case "t":
+      retVal = 0.001;
+      break;
+    case "lb":
+      retVal = 2.205;
+      break;
+    case "oz":
+      retVal = 35.274;
+      break;
+    default:
+      retVal = 0;
+  }
+
+  return retVal;
+}
